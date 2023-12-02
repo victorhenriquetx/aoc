12bbd1b85669341c2922004e060464c72c9f3fd7
Victor Teixeira <henriquevictor96@gmail.com>
2023-12-02 16:51:21 -0300

    clean up

--- 2023/day_2/day2.py
+++ 2023/day_2/day2.py
@@ -2,66 +2,45 @@ import sys
 
 MAX_BLUE = 14
 MAX_RED = 12
-MAX_GREEN = 13
-
+MAX_GREEN = 13    
 
 # Part 1
-def p1(lines):
-    count = 0
-    for line in lines:
-        [tag, game] = line.split(": ")
-        game_id = tag.replace("Game ", "")
-        sets = game.split("; ")
-        poss_set = []
-        for i, set in enumerate(sets):
-            cubes = set.split(", ")
-            possible = []
-            for i, cube in enumerate(cubes):
-                [num, color] = cube.split(" ")
-                if color == "green" and int(num)<=13 or color == "red" and int(num)<=12 or color == "blue" and int(num)<=14: 
-                    possible.append(True)
-                else:
-                    possible.append(False)
-            poss_set.append(False if False in possible else True)
-        # print(poss_set)
-        if False not in poss_set:
-            count+=int(game_id)
-    # print(count)
-
-
+def p1(line):
+    [tag, game] = line.split(": ")
+    game_id = int(tag.replace("Game ", ""))
+    for set in game.split("; "):
+        poss = 1
+        for cube in set.split(", "):
+            [num, color] = cube.split(" ")
+            if color == "green" and int(num)<=13 or color == "red" and int(num)<=12 or color == "blue" and int(num)<=14: 
+                poss *= 1
+            else:
+                poss *= 0
+        game_id *= poss
+    return game_id
 
 # Part 2
-def p2(lines):
-    count = 0
-    for line in lines:
-        [tag, game] = line.split(": ")
-        game_id = tag.replace("Game ", "")
-        sets = game.split("; ")
-        poss_set = []
-        MIN_GREEN, MIN_RED, MIN_BLUE = 1,1,1 
-        for i, set in enumerate(sets):
-            cubes = set.split(", ")
-            possible = []
-            for i, cube in enumerate(cubes):
-                [num, color] = cube.split(" ")
-                if color == "green" and int(num)>MIN_GREEN:
-                    MIN_GREEN = int(num)
-                if color == "red" and int(num)>MIN_RED:
-                    MIN_RED = int(num)  
-                if color == "blue" and int(num)>MIN_BLUE:
-                    MIN_BLUE = int(num) 
-                
-            #calc power here
-        # poss_set.append([MIN_BLUE, MIN_GREEN, MIN_RED])
-        count += MIN_BLUE* MIN_GREEN* MIN_RED
-    print(count)
+def p2(line):
+    game = line.split(": ")[1]
+    min_green, min_red, min_blue = 1,1,1 
+    for set in game.split("; "):
+        for cube in set.split(", "):
+            [num, color] = cube.split(" ")
+            num = int(num)
+            if color == "green" and num>min_green:
+                min_green = num
+            if color == "red" and num>min_red:
+                min_red = num  
+            if color == "blue" and num>min_blue:
+                min_blue = num 
+    return min_blue*min_green*min_red
 
 
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as f, open(sys.argv[2], "w") as out:
         lines = f.read().splitlines()
-        solution = p1(lines)
-        solution2 = p2(lines)
-        # out.write("Part 1: " + str(solution) + "\n")
-        # out.write("Part 2: " + str(solution2) + "\n")
+        solution = sum(map(p1, lines))
+        solution2 = sum(map(p2, lines))
+        out.write("Part 1: " + str(solution) + "\n")
+        out.write("Part 2: " + str(solution2) + "\n")
